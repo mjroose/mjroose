4ecb25bc245f39f44cf799830b9295d2ef38997e
Michael Roose <mjroose@gmail.com>
2020-03-31 16:16:01 -0400

    Add role to user.  Use email instead of password.

--- code/models/user.py
+++ code/models/user.py
@@ -4,16 +4,18 @@ class UserModel(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    password = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    password_hash = db.Column(db.String(80))
+    role = db.Column(db.String(10))
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self, email, password_hash, role):
+        self.email = email
+        self.password_hash = password_hash
+        self.role = role
     
     @classmethod
-    def find_by_username(cls, username):
-        return cls.query.filter_by(username=username).first()
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
 
     @classmethod
     def find_by_id(cls, _id):
